9b1d7398721d9880ab79dd10c18ae2fbb008508f
JhonJurielGulane599 <jhongulane126@gmail.com>
2025-08-18 18:56:56 +0800

    Initial commit

---
+++ resources/views/ninjas/index.blade.php
@@ -0,0 +1,27 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ninja Network | Home</title>
+</head>
+<body>
+    <h2>Currently Available Ninjas</h2>
+
+    @if($greeting == "hello")
+        <p>Hi from inside the if statement</p>
+    @endif
+
+    <ul>
+        <li>
+            <a href="/ninjas/{{$ninjas[0]["id"]}}">
+                {{ $ninjas[0]["name"] }}
+            </a>
+        </li>
+        <li>
+            <a href="/ninjas/{{$ninjas[1]["id"]}}">
+                {{ $ninjas[1]["name"] }}
+            </a>
+        </li>
+    </ul>
+</body>
+</html>
