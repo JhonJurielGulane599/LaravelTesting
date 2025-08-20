c540d79311ec98b0e58f422c6fb0791cd2fe65bf
JhonJurielGulane599 <jhongulane126@gmail.com>
2025-08-20 16:49:06 +0800

    Flash Messages

--- app/Http/Controllers/NinjaController.php
+++ app/Http/Controllers/NinjaController.php
@@ -38,7 +38,7 @@ class NinjaController extends Controller
 
       Ninja::create($validated);
 
-      return redirect()->route('ninjas.index');
+      return redirect()->route('ninjas.index')->with('success', 'Ninja Created!');
     }
 
     public function destroy($id) {
@@ -46,7 +46,7 @@ class NinjaController extends Controller
       $ninja = Ninja::findOrFail($id);
       $ninja->delete();
 
-      return redirect()->route('ninjas.index');
+      return redirect()->route('ninjas.index')->with('success', 'Ninja Deleted!');
     }
 
     // edit() and update() for edit view and update requests
