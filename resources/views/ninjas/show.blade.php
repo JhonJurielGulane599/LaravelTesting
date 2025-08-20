fa04f6bed83efed4da9234a1ebf97df6c03e7bef
JhonJurielGulane599 <jhongulane126@gmail.com>
2025-08-20 15:31:16 +0800

    Delete Requests

--- resources/views/ninjas/show.blade.php
+++ resources/views/ninjas/show.blade.php
@@ -5,13 +5,21 @@
         <p><strong>Skill level:</strong> {{ $ninja->skill }}</p>
         <p><strong>About me:</strong></p>
         <p>{{ $ninja->bio }}</p>
-  </div>
+    </div>
 
-   <div class="border-2 border-dashed bg-white px-4 pb-4 my-4 rounded">
-    <h3>Dojo Information</h3>
-    <p><strong>Dojo name:</strong> {{ $ninja->dojo->name }}</p>
-    <p><strong>Location:</strong> {{ $ninja->dojo->location }}</p>
-    <p><strong>About the Dojo:</strong></p>
-    <p>{{ $ninja->dojo->description }}</p>
-  </div>
+    <div class="border-2 border-dashed bg-white px-4 pb-4 my-4 rounded">
+        <h3>Dojo Information</h3>
+        <p><strong>Dojo name:</strong> {{ $ninja->dojo->name }}</p>
+        <p><strong>Location:</strong> {{ $ninja->dojo->location }}</p>
+        <p><strong>About the Dojo:</strong></p>
+        <p>{{ $ninja->dojo->description }}</p>
+    </div>
+
+    <form action="{{ route('ninjas.destroy', $ninja->id)}}" method="POST">
+        @csrf
+        @method('DELETE')
+        <button type ="submit" class="btn my-4"> Delete Ninja</button>
+
+    </form>
+
 </x-layout>
